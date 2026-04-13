762f314fe27a286546bb7e9f3c8e59465a989c00
Richard James <richard@richardpjames.com>
2026-04-13 13:29:13 +0100

    Fix prisma generation issues

--- src/db/prisma.ts
+++ src/db/prisma.ts
@@ -1,6 +1,6 @@
 // Set up prisma to connect to MariaDB (or MySQL)
 import { PrismaPg } from '@prisma/adapter-pg';
-import { PrismaClient } from '../generated/prisma/client.js';
+import { PrismaClient } from '@prisma/client';
 
 // Connect to the database and set up prisma
 const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL! });
